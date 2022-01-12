cdc4321b5f8a84c9769ddea4617cf7b1a6b88fc1
kiruxaspb <dvoreckovkirill@gmail.com>
2022-01-12 10:07:47 +0300

    access only to the owner + fix

--- beginning/access-mapping.sol
+++ beginning/access-mapping.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.11;
 contract MyContract {
     uint256 public peopleCount = 0;
     mapping(uint => Person) public people;
-    address owner;
+    address owner = 0xEB8Ee7d5723F76e5c1Fd486F47133eaA9d1c5312;
 
     modifier onlyOwner() { // 
         require(msg.sender == owner); // return TRUE or FALSE
@@ -21,8 +21,8 @@ contract MyContract {
     }
 
     function addPerson(string memory _firstname, string memory _lastname) public onlyOwner {
-        incrementCount();
         people[peopleCount] = (Person(_firstname, _lastname));
+        incrementCount();
     }
 
     function incrementCount() internal {
