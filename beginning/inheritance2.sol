fc68e9ff848405b599e44703e57fbaaad4fb7ab5
kiruxaspb <dvoreckovkirill@gmail.com>
2022-01-16 15:25:03 +0300

    passing params

---
+++ beginning/inheritance2.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.11;
+// compile second.sol :)
+contract first {
+    string public name;
+
+    constructor(string memory _name) public {
+        name = _name;
+    }
+}
+
+contract second is first {
+    constructor(string memory _name) first(_name) public {}
+}
