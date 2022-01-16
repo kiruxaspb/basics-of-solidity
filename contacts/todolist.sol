08637ea7a81431460e0f484becfc8a83546384b3
kiruxaspb <dvoreckovkirill@gmail.com>
2022-01-16 13:13:33 +0300

    refactoring

--- contacts/todolist.sol
+++ contacts/todolist.sol
@@ -3,42 +3,57 @@ pragma solidity ^0.8.11;
 
 //insert, update, read from array of structs
 contract TodoList {
-    
+    uint256 public indexTodo = 0;
+    address owner;
+    mapping(uint256 => Todo) public Tasks;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() { // 
+        require(msg.sender == owner); // return TRUE or FALSE
+        _;
+    }
+
     struct Todo {
         string text;
         bool completed;
     }
 
-    Todo[] public todos;
 
-
-    function create(string calldata _text) external {
-        todos.push(Todo({
-            text: _text,
-            completed: false
-        }));
+    function addTask(string memory _text) public onlyOwner {
+        Tasks[indexTodo] = (Todo(_text, false));
+        countTodo();
     }
 
 
-    function removeTask(uint _index) external {
-        delete todos[_index];
+    function countTodo() internal {
+        indexTodo += 1;
     }
 
 
-    function updateText(uint _index, string calldata _text) external {
-            todos[_index].text = _text;
+    function deleteTask(uint256 _index) public onlyOwner {
+        delete Tasks[_index];
+        Tasks[_index].text = "cleaned";
+        Tasks[_index].completed = false;
+    }
+
+
+    function updateText(uint256 _index, string memory _text) public onlyOwner {
+            Tasks[_index].text = _text;
             // todo.text = _text;
             // Todo storage todo = todos[_index]; >> cheaper by gas <<
     }
 
 
-    function get(uint _index) external view returns (string memory, bool) {
-        Todo memory todo = todos[_index]; // can use storage -> a little cheaper of gas
+    function getTodo(uint256 _index) public view onlyOwner returns (string memory, bool) {
+        Todo memory todo = Tasks[_index]; // can use storage -> a little cheaper of gas
         return (todo.text, todo.completed);
     }
 
 
-    function toggleCompleted(uint _index) external { // changing the status of the task by index
-        todos[_index].completed = !todos[_index].completed;
+    function toggleCompleted(uint256 _index) public onlyOwner { // changing the status of the task by index
+        Tasks[_index].completed = !Tasks[_index].completed;
     }
 }
